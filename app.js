217145933726ab3b2f7570fa0703f09e48afde73
gustavo insfran <gujain80@gmail.com>
2023-11-09 22:30:57 -0300

    formularios

--- app.js
+++ app.js
@@ -8,7 +8,12 @@ app.use(express.static(path.join(__dirname,'./public')));
 app.get('/',(req,res)=>{
     res.sendFile(path.join(__dirname,'/views/home.html'))
 })
-
+app.get('/views/register.html',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/register.html'))
+})
+app.get('/views/login.html',(req,res)=>{
+    res.sendFile(path.join(__dirname,'/views/login.html'))
+})
 
 app.listen(3000,()=>{
     console.log('skyNet en linea')
